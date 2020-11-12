2be893d52b2d20081ce54762b19df850a285b1f0
marinetomazi <m.tomazi@hotmail.fr>
2020-11-12 21:31:31 +0100

    iteration 1 on going

---
+++ models/User.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+//fjzipfjme
+const userSchema = new Schema({
+  username: {type: String},
+  password:  {type: String},
+  campus: {type: String, enum:["Madrid","Barcelone","Miami","Paris","Berlin","Amsterdam","Mexico","Sao Paulo","Lisbon" ]},
+  course:{type: String,enum:["Web Dev ","UX UI","Data Analytics",]},
+  image:{type: String}
+}, 
+{
+  timestamps: true
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
